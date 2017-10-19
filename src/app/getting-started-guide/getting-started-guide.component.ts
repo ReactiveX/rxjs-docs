209b790f657fe87223a0e4573b771df5054d7790
ankur <ankur.ratra@powerhouse.nl>
2017-10-19 10:34:46 +0200

    fix(link to external docs): Review comments
    Review comments by the members
    
    https://github.com/ReactiveX/rxjs-docs/issues/33

--- src/app/getting-started-guide/getting-started-guide.component.ts
+++ src/app/getting-started-guide/getting-started-guide.component.ts
@@ -12,9 +12,4 @@ export class GettingStartedGuideComponent implements OnInit {
 
   ngOnInit() {
   }
-
-  goToUrl() {
-    this.document.location.href = 'https://gist.github.com/staltz/868e7e9bc2a7b8c1f754';
-  }
-
 }
