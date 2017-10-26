3d40b1cdd526e4a1c815a278d5a92586c36a5660
Andrew Anderson <Anderson7301@icloud.com>
2017-10-26 09:32:32 -0400

    refactor(Component): Created Shared Module and moved the toolbar to its own component

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -1,55 +1,53 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 import {
+  MatButtonModule,
+  MatCardModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatIconModule,
+  MatListModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatExpansionModule,
+  MatInputModule,
+  MatTooltipModule,
+  MatMenuModule
+} from "@angular/material";
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
     MatButtonModule,
     MatCardModule,
-    MatTableModule,
+    MatExpansionModule,
     MatPaginatorModule,
+    MatTableModule,
     MatIconModule,
+    MatInputModule,
     MatListModule,
     MatSidenavModule,
     MatToolbarModule,
+    MatMenuModule,
+    MatTooltipModule
+  ],
+  providers: [],
+  entryComponents: [],
+  exports: [
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
     MatExpansionModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatIconModule,
     MatInputModule,
-    MatTooltipModule,
-    MatMenuModule
-} from '@angular/material';
-
-
-
-@NgModule({
-    declarations: [],
-    imports: [
-        CommonModule,
-        MatButtonModule,
-        MatCardModule,
-        MatExpansionModule,
-        MatPaginatorModule,
-        MatTableModule,
-        MatIconModule,
-        MatInputModule,
-        MatListModule,
-        MatSidenavModule,
-        MatToolbarModule,
-        MatMenuModule,
-        MatTooltipModule
-    ],
-    providers: [],
-    entryComponents: [],
-    exports: [
-        CommonModule,
-        MatButtonModule,
-        MatCardModule,
-        MatExpansionModule,
-        MatPaginatorModule,
-        MatTableModule,
-        MatIconModule,
-        MatInputModule,
-        MatListModule,
-        MatSidenavModule,
-        MatToolbarModule,
-        MatMenuModule,
-        MatTooltipModule
-    ],
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatTooltipModule
+  ]
 })
-export class SharedModule { }
+export class SharedModule {}
