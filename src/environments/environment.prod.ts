a247d14203e00d0641c6271f77d1192d07425c3d
Andrew Anderson <Anderson7301@icloud.com>
2017-11-29 01:19:17 -0500

    feat(companies): added firebase config to prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDriWFv9xGNiwH_qXxblcUWKhRXY-B7ILU',
+    authDomain: 'rxjs-docs-c893e.firebaseapp.com',
+    databaseURL: 'https://rxjs-docs-c893e.firebaseio.com',
+    projectId: 'rxjs-docs-c893e',
+    storageBucket: 'rxjs-docs-c893e.appspot.com',
+    messagingSenderId: '59456895000'
+  }
 };
