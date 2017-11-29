bc9cea2bc9820802a0797c081e8296a844755c7a
Andrew Anderson <Anderson7301@icloud.com>
2017-11-29 13:14:42 -0500

    feat(environment): Update environment for firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyDriWFv9xGNiwH_qXxblcUWKhRXY-B7ILU',
-    authDomain: 'rxjs-docs-c893e.firebaseapp.com',
-    databaseURL: 'https://rxjs-docs-c893e.firebaseio.com',
-    projectId: 'rxjs-docs-c893e',
-    storageBucket: 'rxjs-docs-c893e.appspot.com',
-    messagingSenderId: '59456895000'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   }
 };
