cfbe5beff83076f5ce110b6f5b412d93d2719606
Hardik Pithva <hardikpthv@gmail.com>
2017-12-14 22:20:28 +0530

    docs(operators): add documentation for sample

--- src/operator-docs/filtering/sample.ts
+++ src/operator-docs/filtering/sample.ts
@@ -1,6 +1,53 @@
 import { OperatorDoc } from '../operator.model';
 
 export const sample: OperatorDoc = {
-  'name': 'sample',
-  'operatorType': 'filtering'
+  name: 'sample',
+  operatorType: 'filtering',
+  signature: 'public sample(notifier: Observable<any>): Observable<T>',
+  marbleUrl: 'http://reactivex.io/rxjs/img/sample.png',
+  parameters: [
+    {
+      name: 'notifier',
+      type: 'Observable<any>',
+      attribute: '',
+      description: `The Observable to use for sampling the source Observable.`
+    }
+  ],
+  shortDescription: {
+    description: `Emits the most recently emitted value from the source Observable whenever another Observable, the notifier, emits.`,
+    extras: [
+      {
+        type: 'Tip',
+        text: `It's like sampleTime, but samples whenever the notifier Observable emits something.`
+      }
+    ]
+  },
+  walkthrough: {
+    description: `
+    <p>
+      Whenever the notifier Observable emits a value or completes,
+      sample looks at the source Observable and emits whichever value
+      it has most recently emitted since the previous sampling,
+      unless the source has not emitted anything since the previous sampling.
+      The notifier is subscribed to as soon as the output Observable is subscribed.
+    </p>
+    `
+  },
+  examples: [
+    {
+      name: 'On every click, sample the most recent "seconds" timer',
+      code: `
+        var seconds = Rx.Observable.interval(1000);
+        var clicks = Rx.Observable.fromEvent(document, 'click');
+        var result = seconds.sample(clicks);
+        result.subscribe(x => console.log(x));
+      `,
+      externalLink: {
+        platform: 'JSBin',
+        url: 'http://jsbin.com/xapiviz/edit?js,console,output'
+      }
+    }
+  ],
+  relatedOperators: ['audit', 'debounce', 'sampleTime', 'throttle'],
+  additionalResources: []
 };
