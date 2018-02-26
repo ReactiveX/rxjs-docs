d5534308c1021885fbb1185fcc6b4ea07f8439d9
José Toledo <jotatoledon@gmail.com>
2018-02-26 02:26:10 +0100

    style(companies): Reordered imports

--- src/app/companies/company.service.ts
+++ src/app/companies/company.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+
 import { COMPANIES_LIST } from './companies-list';
 import { Company } from './companies.model';
 
