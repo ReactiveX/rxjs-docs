46b699fa2faee3c5cc58bf21c52aa5eebd449fd7
coreyschomer <coreyschomer@gmail.com>
2018-03-14 18:34:26 -0600

    docs(observables): add docs for operator : creation - timer
    add docs for operator : creation - timer

--- src/operator-docs/creation/timer.ts
+++ src/operator-docs/creation/timer.ts
@@ -4,19 +4,20 @@ export const timer: OperatorDoc = {
   name: 'timer',
   operatorType: 'creation',
   signature:
-    'public timer(initialDelay: number | Date, period : number = 0, scheduler: IScheduler = async): Observable<number>',
+    'public timer(initialDelay: number | Date, period : number = 0, scheduler: IScheduler = async): Observable',
   parameters: [
     {
       name: 'initialDelay',
       type: 'Number | Date',
       attribute: '',
-      description: `The initial delay time to wait before emitting the first value of 0`
+      description: `The initial delay time specified as a Date object or as an integer denoting
+      milliseconds between emissions to wait before emitting the first value of 0`
     },
     {
       name: 'period',
       type: 'Number',
       attribute: 'optional',
-      description: `The period of time between emissions of the subsequent numbers`
+      description: `The period of time specified as an integer denoting milliseconds between emissions of the subsequent numbers`
     },
     {
       name: 'scheduler',
@@ -35,8 +36,8 @@ export const timer: OperatorDoc = {
     description: `timer returns an Observable that emits an infinite sequence of ascending integers,
     with a constant interval of time, period of your choosing between those emissions.
     The first emission happens after the specified initialDelay.
-    The initial delay may be a Date. By default,
-    this operator uses the async IScheduler to provide a notion of time,
+    The initial delay may be a Date or number of milliseconds to delay.
+    By default, this operator uses the async IScheduler to provide a notion of time,
     but you may pass any IScheduler to it.
     If period is not specified, the output Observable emits only one value, 0.
     Otherwise, it emits an infinite sequence`
@@ -46,18 +47,19 @@ export const timer: OperatorDoc = {
       name:
         'Emits ascending numbers, one every second (1000ms), starting after 3 seconds running for 10 seconds',
       code: `
-          import { timer } from 'rxjs/observable/timer'
-          import { pipe } from 'rxjs';
+          import { timer } from 'rxjs/observable/timer';
           import { take } from 'rxjs/operators';
-          const observable = timer(3000, 1000).pipe(take(10));
-          const subscription = observable.subscribe({
-            next: (n) => console.log(n), // triggered every 1000ms as specified from period param
+
+          const timer$ = timer(3000, 1000).pipe(take(10));
+          const subscription = timer$.subscribe({
+            // delayed 3s (initialDelay) then will output number every 1s (period) for 10s
+            next: (n) => console.log(n),
             complete: () => console.log('complete')
           });
       `,
       externalLink: {
         platform: 'JSBin',
-        url: 'http://jsbin.com/dojatikoqo/embed?html,js,console,output'
+        url: 'http://jsbin.com/ziwarayiwu/1/embed?html,js,console,output'
       }
     }
   ],
