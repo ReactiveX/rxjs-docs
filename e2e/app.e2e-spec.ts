9f0acf9ceab6acfaf70f13c6e99e49e0f91e281f
yashints <yaser@mehraban.com.au>
2018-03-30 22:50:42 +1100

    docs(toolbar): made it responsive

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('rxjs-docs App', () => {
   let page: AppPage;
@@ -10,5 +11,14 @@ describe('rxjs-docs App', () => {
   it('should display title', () => {
     page.navigateTo();
     expect(page.getPageTitle()).toEqual('RxJS Docs');
+  });
+
+  it('should not display title if the window is mobile', () => {
+    browser
+      .manage()
+      .window()
+      .setSize(320, 480);
+    page.navigateTo();
+    expect(page.getPageTitleReference().isDisplayed()).toBeFalsy();
   });
 });
