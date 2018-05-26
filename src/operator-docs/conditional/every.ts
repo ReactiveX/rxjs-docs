295980443b8a2ca94500db2bdf08aadb251f9a8b
Hardik Pithva <hardikpthv@gmail.com>
2018-05-26 15:58:37 +0530

    docs(operators): add documentation for every

--- src/operator-docs/conditional/every.ts
+++ src/operator-docs/conditional/every.ts
@@ -1,6 +1,59 @@
 import { OperatorDoc } from '../operator.model';
 
 export const every: OperatorDoc = {
-  'name': 'every',
-  'operatorType': 'conditional'
+  name: 'every',
+  operatorType: 'conditional',
+  signature: 'public every(predicate: function, thisArg: any): Observable',
+  parameters: [
+    {
+      name: 'predicate',
+      type: 'function',
+      attribute: '',
+      description:
+        'A function for determining if an item meets a specified condition.'
+    },
+    {
+      name: 'thisArg',
+      type: 'any',
+      attribute: 'optional',
+      description: `Optional object to use for 'this' in the callback.`
+    }
+  ],
+  shortDescription: {
+    description:
+      'Returns an Observable that emits whether or not every item of the source satisfies the condition specified.'
+  },
+  walkthrough: {
+    description: `
+      <p>Each value from source, pass predicate returns true otherwise false.</p>
+    `
+  },
+  examples: [
+    {
+      name:
+        'A simple example emitting true if all elements are less than 5, false otherwise',
+      code: `
+        import { every } from 'rxjs/operators';
+        import { of } from 'rxjs/observable/of';
+
+        const source = of(1, 2, 3, 4, 5, 6);
+        const result = source.pipe(
+          //is each valule lesser than 5?
+          every(x => x < 5)
+        );
+        const subscription = result.subscribe(x => console.log(x));
+
+        /*
+        Example console output
+        false
+        */
+      `,
+      externalLink: {
+        platform: 'JSBin',
+        url: 'http://jsbin.com/watafir/embed?js,console'
+      }
+    }
+  ],
+  relatedOperators: [],
+  additionalResources: []
 };
